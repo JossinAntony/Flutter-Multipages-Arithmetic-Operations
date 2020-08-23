692232a564cfab909ac4c5399250f7c73f494265
JossinAntony <jossinde@gmail.com>
2020-08-23 14:52:26 +0200

    add.dart file added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_multipage/pages/add.dart';
 import 'package:flutter_app_multipage/pages/homepage.dart';
 
 void main() {
@@ -8,6 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Homepage();
+    return Add();
   }
 }
---
+++ lib/pages/add.dart
@@ -0,0 +1,80 @@
+
+
+import 'package:flutter/material.dart';
+
+class Add extends StatelessWidget {
+
+  TextEditingController num1Cntrlr = TextEditingController();
+  TextEditingController num2Cntrlr = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ADD'),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(left: 12.0, right: 12.0),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    child: TextField(
+                      controller: num1Cntrlr,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        prefixIcon: Icon(Icons.filter_1),
+                        hintText: 'First number',
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(width: 4.0),
+
+                  Icon(Icons.control_point),
+                  /*Icon(Icons.close),
+                  Icon(Icons.remove),
+                  Icon(Icons.timeline),*/
+
+                  SizedBox(width: 4.0),
+
+                  Flexible(
+                    child: TextField(
+                      controller: num2Cntrlr,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        prefixIcon: Icon(Icons.filter_2),
+                        hintText: 'Second number',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 30.0),
+
+              RaisedButton(
+                onPressed: (){
+                  print(double.parse(num1Cntrlr.text) + double.parse(num2Cntrlr.text));
+                },
+                color: Colors.greenAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Center(child: Text('ADD')),
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
