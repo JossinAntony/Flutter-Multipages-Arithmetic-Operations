9827581fde15fc49404f8109407abae871eaddfb
JossinAntony <jossinde@gmail.com>
2020-08-23 15:01:42 +0200

    subtract.dart file added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_multipage/pages/add.dart';
 import 'package:flutter_app_multipage/pages/homepage.dart';
+import 'package:flutter_app_multipage/pages/subtract.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Add();
+    return Subtract();
   }
 }
