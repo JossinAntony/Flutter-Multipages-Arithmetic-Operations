228875b52df214c41487a08f3f56ca0937b5eed5
JossinAntony <jossinde@gmail.com>
2020-08-23 14:16:26 +0200

    initial commit. homepage.dart created with multiple buttons.

---
+++ android/app/src/main/kotlin/com/jossin/flutter_app_multipage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jossin.flutter_app_multipage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,97 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Operations'),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(left: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width:150,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      //color: Colors.blueAccent,
+                      gradient: LinearGradient(colors: [Color.fromRGBO(221,94,137,1.0), Color.fromRGBO(247,187,151,1.0)]),
+                    ),
+                    child:Center(child: Text('ADD', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ))),
+                  ),
+
+                  SizedBox(width: 10),
+
+                  Container(
+                    width:150,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      //color: Colors.blueAccent,
+                      gradient: LinearGradient(colors: [Color.fromRGBO(7,101,133,1.0), Color.fromRGBO(255,255,255,1.0)]),
+                    ),
+                    child:Center(child: Text('SUBTRACT', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ))),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 10),
+
+              Row(
+                children: <Widget>[
+                  Container(
+                    width:150,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      //color: Colors.blueAccent,
+                      gradient: LinearGradient(colors: [Color.fromRGBO(170,7,107,1.0), Color.fromRGBO(97,4,95,1.0)]),
+                    ),
+                    child:Center(child: Text('MULTIPLY', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ))),
+                  ),
+
+                  SizedBox(width: 10),
+
+                  Container(
+                    width:150,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      //color: Colors.blueAccent,
+                      gradient: LinearGradient(colors: [Color.fromRGBO(229,93,135,1.0), Color.fromRGBO(95,195,228,1.0)]),
+                    ),
+                    child:Center(child: Text('DIVIDE', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ))),
+                  ),
+                ],
+              ),
+
+
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
