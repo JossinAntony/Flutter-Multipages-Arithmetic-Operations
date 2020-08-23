87d878f500b3b474fec69d96ac22090c639c57fd
JossinAntony <jossinde@gmail.com>
2020-08-23 15:48:29 +0200

    navigation to add.dart file from homepage added.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Divide();
+    return MaterialApp(
+      home: Homepage(),
+    );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,6 @@
 
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_multipage/pages/add.dart';
 
 class Homepage extends StatelessWidget {
 
@@ -19,18 +18,21 @@ class Homepage extends StatelessWidget {
             children: <Widget>[
               Row(
                 children: <Widget>[
-                  Container(
-                    width:150,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      //color: Colors.blueAccent,
-                      gradient: LinearGradient(colors: [Color.fromRGBO(221,94,137,1.0), Color.fromRGBO(247,187,151,1.0)]),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => Add())),
+                    child: Container(
+                      width:150,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        //color: Colors.blueAccent,
+                        gradient: LinearGradient(colors: [Color.fromRGBO(221,94,137,1.0), Color.fromRGBO(247,187,151,1.0)]),
+                      ),
+                      child:Center(child: Text('ADD', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.0,
+                      ))),
                     ),
-                    child:Center(child: Text('ADD', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15.0,
-                    ))),
                   ),
 
                   SizedBox(width: 10),
